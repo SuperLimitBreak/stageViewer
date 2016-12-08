5558ce2761eff9b743fe0370bd070422bc1d3149
Allan Callaghan <allan.callaghan@thisisglobal.com>
2016-12-08 19:04:42 +0000

    Considered stage onimentation

--- src/index.js
+++ src/index.js
@@ -38,13 +38,30 @@ const DEFAULT_STAGE_CONFIG = Immutable.fromJS({
         front: {
             style: 'width: 720px; height: 512px; opacity: 0.5;',
             subscriptions: ['main', 'front'],
-            z: 200,
+            y: 256,
+            z: 0,
         },
         rear: {
             style: 'width: 1280px; height: 920px;',
             subscriptions: ['rear'],
-            z: -200,
+            y: 460,
+            z: -400,
         }
+    },
+    stage: {
+        'stage_floor': {
+            style: 'width: 2000px; height: 500px; background-color: #777; border: 1px solid red;',
+            rotation: {x: -90},
+            position: {x: 0, y: 0, z: -250},
+        },
+        'stage_front': {
+            style: 'width: 2000px; height: 100px; background-color: #777; border: 1px solid red;',
+            position: {x: 0, y: -50, z: 0},
+        },
+        'stage_back': {
+            style: 'width: 2000px; height: 1500px; background-color: #777; border: 1px solid red;',
+            position: {x: 0, y: 750, z: -500},
+        },
     },
     lights: {
 
