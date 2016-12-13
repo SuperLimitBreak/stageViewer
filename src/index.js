0f2e60092a9cb1104a23e7b267510741f9c2f5de
Allan Callaghan <allan.callaghan@thisisglobal.com>
2016-12-13 08:10:41 +0000

    Stage representation in 3D

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'core-js/fn/object/assign';
-const Immutable = require('immutable');
 require('normalize.css/normalize.css');
 
 import 'index.html';
@@ -9,6 +8,7 @@ import {SubscriptionSocketReconnect, ScreenMessageRouter, utils} from 'displayTr
 
 import * as THREE from 'three';
 import ThreeMain from './three/main';
+import {initStage} from './stage/stageBuilder';
 
 
 // -----------------------------------------------------------------------------
@@ -25,69 +25,22 @@ body.appendChild(container);
 
 const three = new ThreeMain(container);
 
-// displayTrigger --------------------------------------------------------------
-
 const screenMessageRouter = new ScreenMessageRouter(
     new SubscriptionSocketReconnect()
 );
 
-// Fallback Config -------------------------------------------------------------
-
-const DEFAULT_STAGE_CONFIG = Immutable.fromJS({
-    screens: {
-        front: {
-            style: 'width: 720px; height: 512px; opacity: 0.5;',
-            subscriptions: ['main', 'front'],
-            y: 256,
-            z: 0,
-        },
-        rear: {
-            style: 'width: 1280px; height: 920px;',
-            subscriptions: ['rear'],
-            y: 460,
-            z: -400,
-        }
-    },
-    stage: {
-        'stage_floor': {
-            style: 'width: 2000px; height: 500px; background-color: #777; border: 1px solid red;',
-            rotation: {x: -90},
-            position: {x: 0, y: 0, z: -250},
-        },
-        'stage_front': {
-            style: 'width: 2000px; height: 100px; background-color: #777; border: 1px solid red;',
-            position: {x: 0, y: -50, z: 0},
-        },
-        'stage_back': {
-            style: 'width: 2000px; height: 1500px; background-color: #777; border: 1px solid red;',
-            position: {x: 0, y: 750, z: -500},
-        },
-    },
-    lights: {
-
-    }
-});
-
-function initStage(config) {
-    for (let [screen_name, screen_data] of config.get('screens')) {
-        const screen_div = document.createElement('div');
-        screen_div.style = screen_data.get('style', '');
-        screenMessageRouter.bindScreen(screen_name, screen_div, Array.from(screen_data.get('subscriptions')));
-
-        const CSS3DObject = new THREE.CSS3DObject(screen_div);
-        for (let field of ['x', 'y', 'z']) {
-            CSS3DObject.position[field] = screen_data.get(field) || 0;
-        }
-        three.scene.add(CSS3DObject);
-    }
+function _initStage(config) {
+    return initStage(three, screenMessageRouter, config);
 }
+
+// Fallback Config -------------------------------------------------------------
 
 const config_url = `/data/stage_${utils.getUrlParameter('stage_config') || 'default'}.json`;
 fetch(config_url).then(response => {
     return response.json();
 }).then(data => {
-    initStage(Immutable.fromJS(data));
+    _initStage(Immutable.fromJS(data));
 }).catch(error => {
     console.error(`Unable to load ${config_url} for stage config. Falling back to default`);
-    initStage(DEFAULT_STAGE_CONFIG);
+    _initStage(null);
 });
---
+++ src/stage/stageBuilder.js
@@ -0,0 +1,39 @@
+const Immutable = require('immutable');
+import * as THREE from 'three';
+
+import {DEFAULT_STAGE_CONFIG} from '../data/defaultStageConfig';
+
+const EMPTY_MAP = Immutable.Map();
+
+function createCSS3DObject(data) {
+    const div = document.createElement('div');
+    div.style = data.get('style', '');
+    const CSS3DObject = new THREE.CSS3DObject(div);
+    for (let field of ['x', 'y', 'z']) {
+        CSS3DObject.position[field] = data.get('position', EMPTY_MAP).get(field) || 0;
+    }
+    for (let field of ['x', 'y', 'z']) {
+        CSS3DObject.rotation[field] = ((data.get('rotation', EMPTY_MAP).get(field) || 0) / 360) * 2 * Math.PI;
+    }
+    return CSS3DObject;
+}
+
+export function initStage(three, screenMessageRouter, config) {
+    if (!config) {config = DEFAULT_STAGE_CONFIG;}
+
+    // displayTrigger
+    for (let [screen_name, screen_data] of config.get('screens', EMPTY_MAP)) {
+        const CSS3DObject = createCSS3DObject(screen_data);
+        three.scene.add(CSS3DObject);
+        screenMessageRouter.bindScreen(screen_name, CSS3DObject.element, Array.from(screen_data.get('subscriptions')));
+    }
+
+    // stage
+    for (let [item_name, item_data] of config.get('stage', EMPTY_MAP)) {
+        const CSS3DObject = createCSS3DObject(item_data);
+        three.scene.add(CSS3DObject);
+    }
+
+    // lights
+
+}
