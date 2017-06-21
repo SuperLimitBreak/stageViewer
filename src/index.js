8b465c0dda6e341faa70d726c2f43fc5b19cef7c
Allan Callaghan <allan.callaghan@thisisglobal.com>
2017-06-21 22:42:21 +0100

    Timeline updates on lightingServer update event Image reloading was a pain in the ass. Image scaling was a pain in the ass. Web tech is not all it's cracked up to be ...

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ import {LightManager} from './lights/LightManager';
 
 //const body = document.getElementsByTagName('body').item(0);
 
-const three = new ThreeMain(document.getElementById('3d_scene'));
+const three = new ThreeMain(document.getElementById('three_scene'));
 const subscription_socket = new SubscriptionSocketReconnect();
 const screenMessageRouter = new ScreenMessageRouter(subscription_socket);
-const lightManager = new LightManager(subscription_socket, document.getElementById('timeline_img'));
+const lightManager = new LightManager(subscription_socket, document.getElementById('timeline'));
 
 function _initStage(config) {
     return initStage(three, screenMessageRouter, lightManager, config);
--- src/lights/lightManager.js
+++ src/lights/lightManager.js
@@ -5,11 +5,16 @@ import deviceTypes from './devices';
 
 export class LightManager {
 
-    constructor(subscription_socket, timeline_img) {
-        this.timeline_img = timeline_img;
+    constructor(subscription_socket, timeline_element) {
         this.devices = new Map();
         subscription_socket.addOnMessageListener((msg) => this.onMessage(msg));
         subscription_socket.addSubscriptions(['light_visulisation', 'lights']);
+
+        this.timeline_img = timeline_element.getElementsByTagName('img').item(0);
+        // CSS always enforces correct aspect ratio scaling unless absolute values are specified.
+        // We want to scale 'height' but preserve naturalWidth. We have to enforce this width property manually with js
+        this.timeline_img.onload = () => {this.timeline_img.style.width = `${this.timeline_img.naturalWidth}px`};
+
         this.timeline_name = '';
     }
 
@@ -23,6 +28,7 @@ export class LightManager {
         }
         if (msg.func == 'scan_update_event') {
             if (msg.sequence_files.includes(this.timeline_name)) {
+                this.timeline_img.src = '';
                 this.refresh_timeline();
             }
         }
@@ -33,8 +39,11 @@ export class LightManager {
     }
 
     refresh_timeline() {
-        this.timeline_img.src = `http://localhost:23487/${this.timeline_name}`;
-        console.log(this.timeline_img.src);
+        if (!this.timeline_img.src.includes(this.timeline_name)) {
+            // There is no image.reload(). The only workaround is cachebust
+            // This defeats any attempt to etag cache. (Not happy).
+            this.timeline_img.src = `http://localhost:23487/${this.timeline_name}?${new Date().getTime()}`;
+        }
     }
 
     render(state) {
