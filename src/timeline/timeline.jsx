4aab24d4d601387e49176fd6922ea254a65b5f89
Allan Callaghan <allan.callaghan@thisisglobal.com>
2017-08-10 09:10:18 +0100

    Attempt at selection drag

--- src/timeline/timeline.jsx
+++ src/timeline/timeline.jsx
@@ -41,11 +41,18 @@ export class Timeline extends Component {
             imageWidth: 0,
             selectionStart: 0,
             selectionEnd: 0,
+            selecting: null,
         };
         this._timecode_to_px = this._timecode_to_px.bind(this);
         this._px_to_timecode = this._px_to_timecode.bind(this);
-        this._seek = this._seek.bind(this);
+        this._mouseUp = this._mouseUp.bind(this);
+        this._mouseDown = this._mouseDown.bind(this);
+        this._selectionStart = this._selectionStart.bind(this);
+        this._selectionUpdate = this._selectionUpdate.bind(this);
+        this._selectionEnd = this._selectionEnd.bind(this);
         this.seek = this.seek.bind(this);
+        this.hasSelection = this.hasSelection.bind(this);
+        this.clearSelection = this.clearSelection.bind(this);
         this.setName = this.setName.bind(this);
         this.setCursorPosition = this.setCursorPosition.bind(this);
         this._boundImageObjectNaturalWidth = this._boundImageObjectNaturalWidth.bind(this);
@@ -64,17 +71,69 @@ export class Timeline extends Component {
         this.setState({cursorPosition: timecode});
     }
 
-    _seek(event) {
-        this.seek(this._px_to_timecode(event.clientX));
-    }
     seek(timecode) {
         console.warn('seek must be overridden');
+    }
+
+    hasSelection() {
+        return this.state.selecting!=null || this.state.selectionStart > 0 || this.state.selectionEnd > 0;
+    }
+    clearSelection() {
+        this.setState({selectionStart: 0, selectionEnd: 0, selecting: null});
+    }
+
+    _mouseDown(event) {
+        function bindMarkerDrag(_this, markerName) {
+            if (
+                Math.abs(_this._px_to_timecode(event.clientX) - _this.state[`selection${markerName}`]) < 1
+            ) {
+                _this.setState({selecting: markerName});
+                _this._selectionUpdate(event);
+            }
+        }
+        if (this.hasSelection()) {
+            bindMarkerDrag(this, 'Start');
+            bindMarkerDrag(this, 'End');
+        } else {
+            this.setState({selecting: 'End', selectionStart: this._px_to_timecode(event.clientX)});
+        }
+    }
+    _mouseUp(event) {
+        if (
+            this.state.selecting==null ||
+            (
+                this.state.selecting=='End' &&
+                this._timecode_to_px(
+                    Math.abs(this.state.selectionStart - this.state.selectionEnd)
+                ) < 5
+            )
+         ) {
+            this.clearSelection();
+            this.seek(this._px_to_timecode(event.clientX));
+        }
+        else {
+            this._selectionEnd(event);
+        }
+    }
+    _selectionStart(event) {
+    }
+    _selectionUpdate(event) {
+        if (this.state.selecting) {
+            const state = {};
+            state[`selection${this.state.selecting}`] = this._px_to_timecode(event.clientX);
+            this.setState(state);
+        }
+    }
+    _selectionEnd(event) {
+        if (this.state.selecting) {
+            this._selectionUpdate(event);
+            this.setState({selecting: null});
+        }
     }
 
     _px_to_timecode(px) {
         return px / (this.props.pixelsPerSecond * this.props.zoom);
     }
-
     _timecode_to_px(timecode) {
         return timecode * (this.props.pixelsPerSecond * this.props.zoom);
     }
@@ -87,13 +146,18 @@ export class Timeline extends Component {
 
     render() {
         return (
-            <div className={`timeline`} onClick={this._seek}>
+            <div className={`timeline`}>
                 <img
                     src={`http://${this.props.host}/${this.state.name}?${this.state.cacheBust}`}
                     style={{
                         width: `${this.state.imageWidth * this.props.zoom}px`,
                     }}
                     onLoad={this._boundImageObjectNaturalWidth}
+                    onMouseDown={this._mouseDown}
+                    onMouseLeave={this._selectionEnd}
+                    onMouseOut={this._selectionEnd}
+                    onMouseMove={this._selectionUpdate}
+                    onMouseUp={this._mouseUp}
                 />
                 <div
                     className='selection'
