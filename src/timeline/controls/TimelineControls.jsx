36e0d63968b01ad4bba536f5e3286843b8f26376
Allan Callaghan <calaldees@gmail.com>
2019-05-03 12:10:17 +0100

    Added global keyboard handler for keyboard shortcuts

--- src/timeline/controls/TimelineControls.jsx
+++ src/timeline/controls/TimelineControls.jsx
@@ -4,6 +4,7 @@
 // this component sits above the timeline
 
 import React from 'react';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
 
 import { TrackSelection } from './TrackSelection';
 import { Timecode } from './Timecode';
@@ -16,33 +17,46 @@ export class TimelineControls extends React.Component {
     constructor(props) {
         super(props);
 
-        this.onPlay = this.onPlay.bind(this);
-        this.onPause = this.onPause.bind(this);
-        this.onStop = this.onStop.bind(this);
-        this.onFrameBack = this.onFrameBack.bind(this);
-        this.onFrameForward = this.onFrameForward.bind(this);
+        this.actionPlay = this.actionPlay.bind(this);
+        this.actionPause = this.actionPause.bind(this);
+        this.actionStop = this.actionStop.bind(this);
+        this.actionFrameBack = this.actionFrameBack.bind(this);
+        this.actionFrameForward = this.actionFrameForward.bind(this);
+        this.handleKeyEvent = this.handleKeyEvent.bind(this);
+        this._handleKeyEvent = {
+            ',': this.actionFrameBack,
+            '.': this.actionFrameForward,
+            'home': this.actionStop,
+            'esc': this.actionStop,
+            'clear': this.actionStop,
+            'space': () => (this.props.playing ? this.actionPause : this.actionPlay)(),
+        };
     }
 
-    onPlay() {
+    handleKeyEvent(key, event) {
+        this._handleKeyEvent[key]();
+    }
+
+    actionPlay() {
         this.props.onUpdateState({playing: true});
         this.props.lightsCommand('start_sequence', {timecode: this.props.cursorPosition});
     }
 
-    onPause() {
+    actionPause() {
         this.props.onUpdateState({playing: false});
         this.props.lightsCommand('pause');
     }
 
-    onStop() {
+    actionStop() {
         this.props.onUpdateState({playing: false});
         this.props.lightsCommand('pause');
         this.props.lightsCommand('seek', {timecode: 0});
     }
 
-    onFrameBack() {
+    actionFrameBack() {
         this.props.lightsCommand('seek', {timecode: this.props.cursorPosition, frame_offset: -1});
     }
-    onFrameForward() {
+    actionFrameForward() {
         this.props.lightsCommand('seek', {timecode: this.props.cursorPosition, frame_offset: +1});
     }
 
@@ -55,13 +69,17 @@ export class TimelineControls extends React.Component {
             <div className="timeline_controls">
                 {
                     this.props.playing ?
-                        <button className="timeline_pause" onClick={this.onPause}></button>
+                        <button className="timeline_pause" onClick={this.actionPause}></button>
                         :
-                        <button className="timeline_play" onClick={this.onPlay}></button>
+                        <button className="timeline_play" onClick={this.actionPlay}></button>
                 }
-                <button className="timeline_stop" onClick={this.onStop}></button>
-                <button className="timeline_frame-back" onClick={this.onFrameBack}></button>
-                <button className="timeline_frame-forward" onClick={this.onFrameForward}></button>
+                <button className="timeline_stop" onClick={this.actionStop}></button>
+                <button className="timeline_frame-back" onClick={this.actionFrameBack}></button>
+                <button className="timeline_frame-forward" onClick={this.actionFrameForward}></button>
+                <KeyboardEventHandler
+                    handleKeys={[',', '.', 'home', 'space', 'esc', 'backspace', 'clear']}
+                    onKeyEvent={this.handleKeyEvent}
+                />
 
                 <div className='timing_info'>
                     <div>{this.props.bpm}bpm</div>
