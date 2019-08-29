cc8f3047b19a4608fbea042a55f122d0b5fd8470
Allan Callaghan <calaldees@gmail.com>
2019-08-29 21:25:06 +0100

    Attempt at copying static assets for pixi_particles from displayTrigger

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 'use strict';
 const {buildWebpackCfg} = require('../displayTrigger/display/webpack.config.base.js');
 const webpackCfg = buildWebpackCfg(__dirname);
-webpackCfg.entry.index = 'index.jsx';
 
 const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 webpackCfg.plugins.push(
-    new webpack.ProvidePlugin({
-        "THREE": "three",
-    })
+    new webpack.ProvidePlugin(
+        { THREE: "three" }
+    ),
+    new CopyPlugin([
+        { from: 'node_modules/displayTrigger/src/assets', to: 'assets' },
+    ]),
 );
+
+webpackCfg.entry.index = 'index.jsx';
 
 module.exports = webpackCfg;
